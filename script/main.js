1154674a5c42d46f2799dd73a2cee85c406a120a
Walnut Sleet <zephyrrhdu@gmail.com>
2022-06-26 20:08:09 -0400

    started with the script

---
+++ script/main.js
@@ -0,0 +1,92 @@
+"use strict";
+
+const elements = {
+    answering: {
+        answerText: document.getElementById('answer-text'),
+        answerButton: document.getElementById('submit-answer')
+    },
+    directions: document.getElementById('directions'),
+    info: {
+        capital: document.getElementById('capital'),
+        language: document.getElementById('language'),
+        border: document.getElementById('border'),
+        flag: document.getElementById('flag')
+    }, 
+    start: document.getElementById('start'),
+    choices: document.getElementsByClassName('select'),
+    qLocation: {
+        current: document.getElementById('current'),
+        total: document.getElementById('total'),
+    }
+};
+
+const forQuiz = {
+    type: 'all',
+    countries: null,
+    current: 0,
+}
+
+/* this will set the information section of the page to the current country */
+
+function setInfo() {
+    const curCountry = forQuiz.countries[forQuiz.current];
+    
+    elements.info.capital.innerText = curCountry.capital;
+    elements.info.border.innerText = curCountry.borders;
+    elements.info.language.innerText = Object.values(curCountry.languages);
+    elements.info.flag.href = curCountry.flags.png;
+}
+
+/* This is the setting up of the function for choosing the countries */
+
+function choiceFunction(element) {
+    element.target.style.backgroundColor = 'rgb(59, 87, 245)';
+    for (let button of elements.choices) {
+        if (button !== element.target) {
+            button.style.backgroundColor = 'rgb(0, 47, 255)';
+        };
+    }
+    forQuiz.type = element.target.dataset.value;
+}
+
+for (let button of elements.choices) {
+    button.addEventListener('click', choiceFunction);
+}
+
+/* This is setting the function for the first time click for the start button (changing innerText to Restart) */
+
+function toRestart() {
+    elements.start.innerText = 'Restart';
+    elements.directions.style.display = 'none';
+    elements.start.removeEventListener('click', toRestart);
+}
+
+elements.start.addEventListener('click', toRestart);
+
+/* This is setting the function for the functional click for the start button */
+
+async function startQuiz() {
+    const which = forQuiz.type === 'all' ? 'all' : 'region/' + forQuiz.type;
+    const url = 'https://restcountries.com/v3.1/' + which;
+
+    try {
+        const response = await fetch(url);
+
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            const jsonResponseFiltered = jsonResponse.filter(country => country.independent);
+
+            forQuiz.countries = jsonResponseFiltered.sort(() => (Math.random() > .5) ? 1 : -1); 
+            
+            setInfo();
+        } else {
+            throw new Error('Request failed!')
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+elements.start.addEventListener('click', startQuiz);
+
+
